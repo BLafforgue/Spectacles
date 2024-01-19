46e62b1c9666f2fb2632510e78ce5f7a7d52995c
Baptiste LAFFORGUE <baptiste.lafforgue@outlook.fr>
2024-01-19 15:03:15 +0100

    Ajout élements liste dates

--- spectacle.js
+++ spectacle.js
@@ -1,6 +1,17 @@
 const btnAdd = document.getElementById("add")
 btnAdd.addEventListener("click", addTicket)
 
+// Ajout de dates dans le select
+const listDates = ["2023-12-18", "2023-12-22", "2023-12-25", "2023-12-25", "2023-12-29"]
+const selectDate = document.getElementById("selectDate")
+for (const listDate of listDates) {
+    var optionElement = document.createElement("option");
+
+    optionElement.textContent = listDate
+    optionElement.value = listDate
+    selectDate.appendChild(optionElement)
+}
+
 // Ajouter un ticket
 function addTicket(event) {
     const originalTicket = document.getElementById("ticket")
