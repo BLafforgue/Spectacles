00a71c0f079313d29751a704edbbd27ae8235b5c
Baptiste LAFFORGUE <baptiste.lafforgue@outlook.fr>
2024-01-19 14:26:55 +0100

    Correction formulaire

--- spectacle.js
+++ spectacle.js
@@ -2,13 +2,14 @@ const btnAdd = document.getElementById("add")
 btnAdd.addEventListener("click", addTicket)
 
 // Ajouter un ticket
-function addTicket() {
+function addTicket(event) {
     const originalTicket = document.getElementById("ticket")
     const newTicket = originalTicket.cloneNode(true);
     newTicket.classList.add("ticket");
-    document.body.appendChild(newTicket);
+    document.querySelector("form").appendChild(newTicket)
+    event.preventDefault()
 
-    // Ajout ju bouton de suppression
+    // Ajout du bouton de suppression
     const btnRemove = document.createElement("button");
     btnRemove.textContent = "Supprimer";
     btnRemove.addEventListener("click", () => removeTicket(newTicket));
